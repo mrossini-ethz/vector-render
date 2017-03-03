84b72089c1f0eea6a2d2d18dfeb5cfb989970e73
Marco Rossini <marco.rossini@phys.ethz.ch>
2017-03-03 22:35:32 +0100

    Added the metainformation to the add-on.

--- vector-render.py
+++ vector-render.py
@@ -1,7 +1,43 @@
+# Blender Add-on: Vector Render
+# Copyright (C) 2017  Marco Rossini
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# This Blender plugin is based on the research paper "Recovery of Intrinsic
+# and Extrinsic Camera Parameters Using Perspective Views of Rectangles" by
+# T. N. Tan, G. D. Sullivan and K. D. Baker, Department of Computer Science,
+# The University of Reading, Berkshire RG6 6AY, UK, Email: T.Tan@reading.ac.uk,
+# from the Proceedings of the British Machine Vision Conference, published by
+# the BMVA Press.
+
 import bpy
 import mathutils
 from math import cos, degrees, radians, sqrt, acos, pi
 from random import choice
+
+bl_info = {
+    "name": "Vector Render",
+    "author": "Marco Rossini",
+    "version": (0, 0, 1),
+    "warning": "This is an unreleased development version.",
+    "blender": (2, 7, 0),
+    "location": "Properties > Render > Vector Render",
+    "description": "Renders the camera view to a vector graphic such as SVG.",
+    "tracker_url": "https://github.com/mrossini-ethz/vector-render/issues",
+    "support": "COMMUNITY",
+    "category": "Render"
+}
 
 # GENERIC FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------------------
 
